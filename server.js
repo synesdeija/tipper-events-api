1ff4f6ead56934a06798955ff04225097ec7e049
synesdeija <thedeija@gmail.com>
2022-07-25 13:25:16 -0500

    getting back into this project

--- server.js
+++ server.js
@@ -1,95 +1,88 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
+require('dotenv').config()
 
-app.use(cors())
 
-const event = {
-    'redRocks21': {
-        'type': 'outdoor',
-        'venue': 'Red Rocks Amphitheater',
-        'eventName': 'Tipper Red Rocks 2021',
-        'location': 'Morrison, Colorado',
-        'setCount': 'two',
-        'dates': 'Friday and Saturday, August 6-7, 2021'
-    },
-    'saengerTheater': {
-        'type': 'theater',
-        'venue': 'Saenger Theater',
-        'eventName': 'Tipper & Friends',
-        'location': 'New Orleans, Louisiana',
-        'setCount': 'three',
-        'dates': 'January 3-5, 2020'
-    },
-    'fullMoonReturn': {
-        'type': 'outdoor',
-        'venue': 'Spirit of Suwannee Music Park',
-        'eventName': 'Tipper & Friends: The Full Moon Return',
-        'location': 'Live Oak, Florida',
-        'setCount': 'three',
-        'dates': 'April 19-21, 2019'
-    },
-    'astralLights': {
-        'type': 'outdoor',
-        'venue': 'Astral Valley Music Park',
-        'eventName': 'Astral Lights',
-        'location': 'French Village, Missouri',
-        'setCount': 'two',
-        'dates': 'September 28-29, 2018'
-    },
-    'fourThreeTwoOne': {
-        'type': 'outdoor',
-        'venue': 'Astral Valley Music Park',
-        'eventName': '4,3,2,1',
-        'location': 'French Village, Missouri',
-        'setCount': 'four',
-        'dates': 'August 18-21,2017'
-    },
-    'nola1': {
-        'type': 'indoor/theater',
-        'venue': "Tipitina'/s and The Orpheum Theater",
-        'eventName': '4,5,6,7',
-        'location': 'New Orleans, Louisiana',
-        'setCount': 'four',
-        'dates': 'January 4-7, 2018'
-    },
-    'suwannee2017': {
-        'type': 'outdoor',
-        'venue': 'Spirit of Suwannee Music Park',
-        'eventName': 'The Nocturnal Edition',
-        'location': 'Spirit of Suwannee Music Park',
-        'setCount': 'three',
-        'dates': 'May 19-20, 2017'
-    },
-    'unknown': {
-        'type': 'unknown',
-        'venue': 'unknown',
-        'eventName': 'unknown',
-        'location': 'unknown',
-        'setCount': 'unknown',
-        'dates': 'unknown'
-    }
-}
+let db,
+    dbConnectionStr =
+    'mongodb+srv://deija1:buttstuff@cluster0.1w4hgfp.mongodb.net/?retryWrites=true&w=majority',
+    dbName = 'tipper-events-api'
 
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/index.html')
+    db.collection('tipper-events-api').find().sort({ likes: -1 }).toArray()
+        .then(data => {
+            response.render('index.ejs', { info: data })
+        })
+        .catch(error => console.error(error))
 })
 
-app.get('/tipper-events-api/', (request, response) => {
-    response.json(event)
+app.post('/addEvent', (request, response) => {
+    db.collection('tipper-events-api').insertOne({
+            event: request.body.event,
+            eventName: request.body.eventName,
+            type: request.body.type,
+            venue: request.body.venue,
+            location: request.body.location,
+            setCount: request.body.setCount,
+            dates: request.body.dates,
+            likes: 0
+        })
+        .then(result => {
+            console.log('Event Added!')
+            response.redirect('/')
+        })
+        .catch(error => console.error(error))
 })
 
-app.get('/tipper-events-api/:name', (request, response) => {
-    const name = request.params.name.toLowerCase()
-    if (event[name]) {
-        response.json(event[name])
-    } else {
-        response.json(event['unknown'])
-    }
+app.put('/addOneLike', (request, response) => {
+    db.collection('tipper-events-api').updateOne({
+            event: request.body.event,
+            eventName: request.body.eventName,
+            type: request.body.type,
+            venue: request.body.venue,
+            location: request.body.location,
+            setCount: request.body.setCount,
+            dates: request.body.dates,
+            likes: request.body.likesS
+        }, {
+            $set: {
+                likes: request.body.likesS + 1
+            }
+        }, {
+            sort: { _id: -1 },
+            upsert: true
+        })
+        .then(result => {
+            console.log('Added One Like')
+            response.json('Like Added')
+        })
+        .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteEvent', (request, response) => {
+    db.collection('tipper-events-api').deleteOne({ event: request.body.eventS })
+        .then(result => {
+            console.log('Event Deleted')
+            response.json('Event Deleted')
+        })
+        .catch(error => console.error(error))
+
 })
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log(`The server is running on port ${PORT}! Betta Go Catch It!`)
+    console.log(`Server running on port ${PORT}`)
 })
